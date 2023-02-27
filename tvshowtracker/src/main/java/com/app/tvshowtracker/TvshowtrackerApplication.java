a2b9e9094ab3bb972fed46c5f7722e064ca8b490
Mohammad Sulaiman <74812294+MohammadSulaimanHyder@users.noreply.github.com>
2023-02-27 19:08:12 +0530

    first commit

---
+++ tvshowtracker/src/main/java/com/app/tvshowtracker/TvshowtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.app.tvshowtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvshowtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvshowtrackerApplication.class, args);
+	}
+
+}
---
+++ tvshowtracker/src/test/java/com/app/tvshowtracker/TvshowtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tvshowtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvshowtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
